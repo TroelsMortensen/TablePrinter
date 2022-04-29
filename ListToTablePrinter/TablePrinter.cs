03e2af345476adee92bcc9f1188a1ffa9d60e7a3
TRMO <tromortensen@hotmail.com>
2022-04-29 12:49:54 +0200

    Initial commit

---
+++ ListToTablePrinter/TablePrinter.cs
@@ -0,0 +1,97 @@
+﻿using System.Reflection;
+
+namespace ListToTablePrinter
+{
+    public class TablePrinter
+    {
+        public static void Print<T>(IEnumerable<T> list)
+        {
+            var properties = typeof(T).GetProperties();
+            Dictionary<string, int> columnLengths = InitializeInfo(list, properties);
+            var tableHeader = CreateTableHeader<T>(properties, columnLengths);
+
+
+            string top = "";
+            string bottom = "";
+            for (int i = 0; i < tableHeader.Length; i++)
+            {
+                top += "_";
+                bottom += "-";
+            }
+
+            tableHeader += bottom ;
+            var table = CreateTable(list, tableHeader, properties, columnLengths);
+
+            Console.WriteLine("\n"+top);
+            Console.WriteLine(table);
+            Console.WriteLine(bottom +"\n");
+        }
+
+        private static string CreateTable<T>(IEnumerable<T> list, string tableHeader, PropertyInfo[] properties, Dictionary<string, int> columnLengths)
+        {
+            string table = tableHeader + "\n";
+            foreach (T item in list)
+            {
+                string row = "";
+                foreach (PropertyInfo prop in properties)
+                {
+                    string rowItem = prop.GetValue(item) != null ? prop.GetValue(item)?.ToString() : "null";
+                    while (rowItem.Length <= columnLengths[prop.Name])
+                    {
+                        rowItem += " ";
+                    }
+
+                    row += rowItem + "| ";
+                }
+
+                row = row.TrimEnd('|');
+                table += row + "\n";
+            }
+
+            table = table.TrimEnd('\n');
+            return table;
+        }
+
+        private static string CreateTableHeader<T>(PropertyInfo[] properties, Dictionary<string, int> columnLengths)
+        {
+            string tableHeader = "";
+            foreach (PropertyInfo prop in properties)
+            {
+                string headerItem = prop.Name;
+                while (headerItem.Length <= columnLengths[prop.Name])
+                {
+                    headerItem += " ";
+                }
+
+                tableHeader += headerItem + "| ";
+            }
+
+            tableHeader = tableHeader.TrimEnd('|');
+            return tableHeader;
+        }
+
+        private static Dictionary<string, int> InitializeInfo<T>(IEnumerable<T> list, PropertyInfo[] properties)
+        {
+            Dictionary<string, int> columnLengths = new Dictionary<string, int>();
+
+            foreach (PropertyInfo prop in properties)
+            {
+                columnLengths.Add(prop.Name, prop.Name.Length);
+            }
+
+            // Finding column lengths
+            foreach (T item in list)
+            {
+                foreach (PropertyInfo prop in properties)
+                {
+                    if (prop.GetValue(item) != null && columnLengths[prop.Name] < prop.GetValue(item).ToString().Length)
+                    {
+                        columnLengths[prop.Name] = prop.GetValue(item).ToString().Length;
+                    }
+                }
+            }
+
+            return columnLengths;
+        }
+    }
+}
